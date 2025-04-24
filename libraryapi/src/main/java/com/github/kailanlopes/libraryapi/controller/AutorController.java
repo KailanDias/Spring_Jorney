99b9c4fc090e3098dbf5d5e0286c87f34eae914a
Kailan <kailanlopes1103@gmail.com>
2025-04-23 23:07:37 -0300

    Deletar e Pesquisar

--- libraryapi/src/main/java/com/github/kailanlopes/libraryapi/controller/AutorController.java
+++ libraryapi/src/main/java/com/github/kailanlopes/libraryapi/controller/AutorController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -32,7 +34,7 @@ public class AutorController {
 
     @GetMapping("{id}")
     public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id) {
-        var idAutor = UUID.fromString(id);
+        var idAutor = UUID.fromString(id); // Transforma o id do autor em UUID
         Optional<Autor> autorOptional= autorService.obterPorId(idAutor);
         if(autorOptional.isPresent()){
             Autor autor = autorOptional.get();
@@ -41,5 +43,26 @@ public class AutorController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id); // Transforma o id do autor em UUID
+        Optional<Autor> autorOptional= autorService.obterPorId(idAutor);
+
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        autorService.deletar(autorOptional.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(@RequestParam(value = "nome", required = false) String nome, @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
+        List<Autor> resultadoPesquisa = autorService.pesquisar(nome, nacionalidade);
+        List<AutorDTO> lista = resultadoPesquisa.stream().map(autor -> new AutorDTO(autor.getId(),autor.getNome(),autor.getDataNascimento(),autor.getNacionalidade())).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
+    }
+
+
 
 }
--- libraryapi/src/main/java/com/github/kailanlopes/libraryapi/service/AutorService.java
+++ libraryapi/src/main/java/com/github/kailanlopes/libraryapi/service/AutorService.java
@@ -4,6 +4,7 @@ import com.github.kailanlopes.libraryapi.model.Autor;
 import com.github.kailanlopes.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -22,5 +23,23 @@ public class AutorService {
 
     public Optional<Autor> obterPorId(UUID id){
         return autorRepository.findById(id);
+    }
+
+    public void deletar(Autor autor){
+        autorRepository.delete(autor);
+    }
+
+    public List<Autor> pesquisar(String nome, String nacionalidae){
+
+        if (nome != null && nacionalidae != null){ // se passou as duas
+            return autorRepository.findByNomeAndNacionalidade(nome, nacionalidae);
+        }
+        if (nome != null){ // se passou somente o nome
+            return autorRepository.findByNome(nome);
+        }
+        if (nacionalidae != null){ // se passou somente a nacionalidade
+            return autorRepository.findByNacionalidade(nacionalidae);
+        }
+        return autorRepository.findAll(); // se nao passou nada
     }
 }
